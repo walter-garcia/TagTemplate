d19ef8fdbf0dc9418d7a552e429452fc8a64d67d
Walter Marcel Barbosa Garcia <waltermbgarcia@hotmail.com>
2019-08-07 15:20:20 -0300

    inital commit

---
+++ index.php
@@ -0,0 +1,100 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <link href="https://fonts.googleapis.com/css?family=Oswald:200,300,400,500,600,700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.9.0/css/all.min.css">
+    <link rel="stylesheet" href="assets/css/styles.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Template</title>
+  </head>
+  <body>
+    <header class="header">
+      <h1>ORGANIZE</h1>
+      <h2>Your Life</h2>
+    </header>
+    <main class="main">
+      <div class="content">
+        <nav class="modules">
+          <div class="module green">
+            <h3>Label Name</h3>
+            <ul>
+              <li><a href="template.php">Whatever you want here</a></li>
+              <li><a href="#">Whatever you want here</a></li>
+              <li><a href="#">Whatever you want here</a></li>
+            </ul>
+          </div>
+          <div class="module red">
+            <h3>Label Name</h3>
+            <ul>
+              <li><a href="#">Whatever you want here</a></li>
+              <li><a href="#">Whatever you want here</a></li>
+              <li><a href="#">Whatever you want here</a></li>
+            </ul>
+          </div>
+          <div class="module blue">
+            <h3>Label Name</h3>
+            <ul>
+              <li><a href="#">Whatever you want here</a></li>
+              <li><a href="#">Whatever you want here</a></li>
+              <li><a href="#">Whatever you want here</a></li>
+            </ul>
+          </div>
+          <div class="module purple">
+            <h3>Label Name</h3>
+            <ul>
+              <li><a href="#">Whatever you want here</a></li>
+              <li><a href="#">Whatever you want here</a></li>
+              <li><a href="#">Whatever you want here</a></li>
+            </ul>
+          </div>
+          <div class="module orange">
+            <h3>Label Name</h3>
+            <ul>
+              <li><a href="#">Whatever you want here</a></li>
+              <li><a href="#">Whatever you want here</a></li>
+              <li><a href="#">Whatever you want here</a></li>
+            </ul>
+          </div>
+          <div class="module dark-green">
+            <h3>Label Name</h3>
+              <ul>
+                <li><a href="#">Whatever you want here</a></li>
+                <li><a href="#">Whatever you want here</a></li>
+                <li><a href="#">Whatever you want here</a></li>
+              </ul>
+            </div>
+          <div class="module dark-red">
+            <h3>Label Name</h3>
+            <ul>
+              <li><a href="#">Whatever you want here</a></li>
+              <li><a href="#">Whatever you want here</a></li>
+              <li><a href="#">Whatever you want here</a></li>
+            </ul>
+          </div>
+          <div class="module dark-blue">
+            <h3>Label Name</h3>
+            <ul>
+              <li><a href="#">Whatever you want here</a></li>
+              <li><a href="#">Whatever you want here</a></li>
+              <li><a href="#">Whatever you want here</a></li>
+            </ul>
+          </div>
+          <div class="module dark-purple">
+            <h3>Label Name</h3>
+            <ul>
+              <li><a href="#">Whatever you want here</a></li>
+              <li><a href="#">Whatever you want here</a></li>
+              <li><a href="#">Whatever you want here</a></li>
+            </ul>
+          </div>
+        </nav>
+      </div>
+    </main>
+    <footer class="footer">
+    <i class="far fa-copyright"></i> <?= date('Y'); ?> - Made with ❤️
+    </footer>
+
+  </body>
+</html>
